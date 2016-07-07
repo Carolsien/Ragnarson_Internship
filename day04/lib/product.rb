25b6f3e68e05a9180b98ef4501e57737439857e0
Karolina <karolina.rostek.93@gmail.com>
2016-07-07 15:31:43 +0200

    product price tests

--- day04/lib/product.rb
+++ day04/lib/product.rb
@@ -11,12 +11,28 @@ class Product
     @name = set_name
   end
 
+  def price
+    @price = set_price
+  end
+
   private
 
   def set_name
     if @name.class == String
       if @name.length > 2
         @name
+      else
+        raise ArgumentError
+      end
+    else
+      raise ArgumentError
+    end
+  end
+
+  def set_price
+    if @price.is_a?(Numeric)
+      if @price > 0 && @price < 10000
+        @price
       else
         raise ArgumentError
       end
--- day04/spec/unit/product_spec.rb
+++ day04/spec/unit/product_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Product do
   context "#name" do
 
     it "returns proper value when name foo given" do
-      expect(Product.new("foo", nil, nil).name).to eql("foo")
+      expect(Product.new("fooo", nil, nil).name).to eql("fooo")
     end
 
     it "raise error when name is empty" do
@@ -55,5 +55,57 @@ RSpec.describe Product do
         Product.new("ssss", nil, nil).name
       }.not_to raise_error
     end
+  end
+
+
+  context "#price" do
+
+    it "returns 12 when 12 given" do
+      expect(Product.new(nil, 12, nil).price).to eq(12)
+    end
+
+    it "raise error when string given" do
+      expect{
+        Product.new(nil, "aaa", nil).price
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raise error when number with comma given" do
+      expect{
+        Product.new(nil, 12,2 , nil).price
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raise error when number 0 given" do
+      expect{
+        Product.new(nil, 0 , nil).price
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raise error when number 10001 given" do
+      expect{
+        Product.new(nil, 10001 , nil).price
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raise error when number -2 given" do
+      expect{
+        Product.new(nil, -2 , nil).price
+      }.to raise_error(ArgumentError)
+    end
+
+    it "does not raise error when number with . given" do
+      expect{
+        Product.new(nil, 12.3 , nil).price
+      }.not_to raise_error
+    end
+
+    it "does not raise error when number given" do
+      expect{
+        Product.new(nil, 12, nil).price
+      }.not_to raise_error
+    end
+
+
   end
 end
