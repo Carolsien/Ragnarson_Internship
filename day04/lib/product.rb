38a7344d86b90bbc7de7747bc67d9b5c465b3d04
Karolina <karolina.rostek.93@gmail.com>
2016-07-07 13:11:48 +0200

    product name tests

--- day04/lib/product.rb
+++ day04/lib/product.rb
@@ -6,4 +6,18 @@ class Product
     @price = price
     @quantity = quantity
   end
+
+  def name
+    @name = set_name
+  end
+
+  private
+
+  def set_name
+    if @name.class == String
+      @name
+    else
+      raise ArgumentError
+    end
+  end
 end
--- day04/spec/unit/product_spec.rb
+++ day04/spec/unit/product_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Product do
       Product.new()
     }.to raise_error(ArgumentError)
   end
+
+  context "#name" do
+
+    it "returns proper value when name given foo " do
+      expect(Product.new("foo", nil, nil).name).to eql("foo")
+    end
+
+    it "does not raise error when string given" do
+      expect{
+        Product.new("foo", nil, nil)
+      }.not_to raise_error
+    end
+
+    it "raise error when number given" do
+      expect{
+        Product.new(9, nil, nil).name
+      }.to raise_error(ArgumentError)
+    end
+  end
 end
