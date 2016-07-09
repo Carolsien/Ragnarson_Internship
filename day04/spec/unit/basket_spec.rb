e1d669cbaf53011c1164df2e1f96e3ca03b3da5e
Karolina <karolina.rostek.93@gmail.com>
2016-07-09 23:13:19 +0200

    add product tests

--- day04/spec/unit/basket_spec.rb
+++ day04/spec/unit/basket_spec.rb
@@ -2,6 +2,11 @@ require_relative '../../lib/basket'
 
 RSpec.describe Basket do
 
+  it "describe an Array" do
+    expect(Basket.create_basket).to be_a_kind_of(Array)
+  end
+
+
   it "does not raise an error" do
     expect{
       Basket.new
--- day04/spec/unit/product_spec.rb
+++ day04/spec/unit/product_spec.rb
@@ -2,16 +2,32 @@ require_relative '../../lib/product.rb'
 
 RSpec.describe Product do
 
-  it "has 3 params" do
-    expect{
-      Product.new(nil, nil, nil)
-    }.not_to raise_error
-  end
+  describe ".new" do
+    let (:product) {Product.new("omg",12.2, 3)}
 
-  it "has 0 params" do
-    expect{
-      Product.new()
-    }.to raise_error(ArgumentError)
+    it "returns instance of product class" do
+      expect(Product.new("omg", 12.2, 3)).to be_a_kind_of(Product)
+    end
+
+  it "initializes properly" do
+    expect(product.name).to eql("omg")
+    expect(product.price).to eql(12.2)
+    expect(product.quantity).to eql(3)
+  end
+end
+
+  context "has params" do
+    it "has 3 params" do
+      expect{
+        Product.new(nil, nil, nil)
+      }.not_to raise_error
+    end
+
+    it "has 0 params" do
+      expect{
+        Product.new()
+      }.to raise_error(ArgumentError)
+    end
   end
 
   context "#name" do
