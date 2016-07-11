046ccf883367da64d71800f9426dd3cb00f3c10f
Karolina <karolina.rostek.93@gmail.com>
2016-07-11 15:27:30 +0200

    add base of the app

---
+++ day06/app.rb
@@ -0,0 +1,25 @@
+require "sinatra/base"
+require_relative "lib/product"
+
+class Products
+  def call
+    [
+      Product.new(id: 1, name: "tea"),
+      Product.new(id: 2, name: "coffee")
+    ]
+  end
+end
+
+class App < Sinatra::Base
+
+  get "/" do
+    @products = Products.new.call
+    erb :index
+  end
+
+  get "/products/:id" do
+    @product = Products.new.call.find{ |product| product.id.to_i == params[:id].to_i}
+    erb :show
+  end
+
+end
